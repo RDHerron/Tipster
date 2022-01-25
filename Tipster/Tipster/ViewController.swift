c9030809c60649d47d873865ec5b5fe8bdb50e07
RDHerron <rherron@bulldogs.aamu.edu>
2022-01-24 23:17:33 -0600

    Added project files

---
+++ Tipster/Tipster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Rahmat Herron on 1/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
